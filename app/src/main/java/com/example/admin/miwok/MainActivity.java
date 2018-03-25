96ce7a1f4c98721d92599a2a197717e246df25c2
darksister <anujabhajibhakre1997@gmail.com>
2018-03-26 00:26:05 +0530

    Third commit

--- app/src/main/java/com/example/admin/miwok/MainActivity.java
+++ app/src/main/java/com/example/admin/miwok/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Toast.makeText(this,"In Main JAva File",Toast.LENGTH_SHORT).show();
     }
 }
